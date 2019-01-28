cce58704e1fcb01db57b07d43a956c838549092e
Kenni Holm <kenn5363@edu.eal.dk>
2019-01-28 16:39:06 +0100

    Initial skeleton

--- Innovation2GUI/ØkonomiWindow.xaml.cs
+++ Innovation2GUI/ØkonomiWindow.xaml.cs
@@ -10,25 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Innovation2GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ØkonomiWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ØkonomiWindow : Window
     {
-        public MainWindow()
+        public ØkonomiWindow()
         {
             InitializeComponent();
         }
 
-        private void ØkonomiBtn_Click(object sender, RoutedEventArgs e)
+        private void TilbageBtn_Click(object sender, RoutedEventArgs e)
         {
-            ØkonomiWindow øko = new ØkonomiWindow();
-            øko.Show();
+            MainWindow main = new MainWindow();
+            main.Show();
             this.Close();
         }
     }
